87daa2198ea559c3a189ed18b0571a34b06dbecb
Aleksandr Golovin <wahahaloal@gmail.com>
2025-06-30 06:49:24 +0300

    Move cookie submit button back to BasePage and add arg locator to it method

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,7 +2,6 @@ import allure
 from pages.base_page import BasePage
 from locators.main_page_locators import MainPageLocators
 from locators.general_locators import GeneralLocators
-from selenium.common.exceptions import TimeoutException
 
 
 class MainPage(BasePage):
@@ -24,17 +23,9 @@ class MainPage(BasePage):
     
     @allure.step('Проверка пары вопрос-ответ')
     def check_question_and_answer(self, index, question_input, answer_input):
-        self.safe_submit_cookie_button()
+        self.safe_submit_cookie_button(GeneralLocators.COOKIE_BUTTON_LOCATOR)
         question_text = self.get_question_text(index)
         self.click_to_question(index)
         answer_text = self.get_answer_text(index)
         result = question_text == question_input and answer_text == answer_input
         return result
-    
-    @allure.step('Подтверждение использования cookie')
-    def safe_submit_cookie_button(self):
-        "Подтвердить использование кук (безопасно)"
-        try:
-            self.click_to_element(GeneralLocators.COOKIE_BUTTON_LOCATOR)
-        except TimeoutException:
-            return
